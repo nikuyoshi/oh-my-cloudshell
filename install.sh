6cd2e4595ac656722b5ea1747eaadfe8e3343aab
Hiroki Uchida <nikuyoshi@gmail.com>
2020-12-29 10:21:53 +0900

    Modify install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum install -y ansible
+git clone https://github.com/nikuyoshi/dev-pc-provisioning.git
+ansible-playbook dev-pc-provisioning/cloudshell.yml
