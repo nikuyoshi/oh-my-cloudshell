ad89de423885efb199856f8de15acaed857ff8ba
Hiroki Uchida <nikuyoshi@gmail.com>
2020-12-29 11:40:26 +0900

    Modify install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+WORKSPACE_DIR=~/dev/workspace
 
+cd ~
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum install -y ansible
+mkdir -p $WORKSPACE_DIR
+cd $WORKSPACE_DIR
 git clone https://github.com/nikuyoshi/dev-pc-provisioning.git
 ansible-playbook dev-pc-provisioning/cloudshell.yml
